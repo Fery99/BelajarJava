df3fcfa2e92100773f2674475d455a83dd16d100
fery sadewa <ferysadewaa@gmail.com>
2023-09-04 09:13:52 +0700

    penambahan Variable Arrayas dan di Loop For

--- Buatbaru.java
+++ Buatbaru.java
@@ -1,7 +1,13 @@
 package ProjectTaik;
 
+import java.util.Arrays;
 public class Buatbaru {
     public static void main(String[] args) {
+        int [] Array1 = {2,3,4,5,6,7,8,91};
+            for( int i = 0; i < Array1.length; i++ ){
+                System.out.println("Ini disebut Aray" + Arrays.toString(Array1) );
+            }
+
         System.out.println("okeoke");
     }
 }
